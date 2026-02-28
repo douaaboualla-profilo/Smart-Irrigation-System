c297f45904cec0608a69fe88a4a25214b9b5e7df
Boualla Douaa <douaa.boualla@g.enp.edu.dz>
2026-03-01 00:31:46 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Dashboard from 'Dashboard';
-import ControlPanel from 'ControlPanel';
-import Analytics from 'Analytics';
+import React from './react';
+import Dashboard from './Dashboard';
+import ControlPanel from './ControlPanel';
+import Analytics from './Analytics';
 
 function App() {
   return (
